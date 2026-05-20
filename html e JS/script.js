faa8a11878606d446d359a6f530a42f8e2622c88
Preixteitiu <isaquesarev@gmail.com>
2026-05-19 21:16:02 -0300

    atividad

---
+++ html e JS/script.js
@@ -0,0 +1,6 @@
+function mudarTexto() {
+  // vai no HTML e pega o elemento pelo id
+  // .innterHTML =  altere o html para o que eu escrever aqui
+  document.getElementById("texto").innerHTML =
+    "Texto novo ao clicar no botao :D";
+}
